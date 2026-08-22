5adac14871361653b84087b234ed4be1452058b7
winterknife <winterknife@protonmail.com>
2026-08-22 18:21:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,11 +36,11 @@ if sys.version_info < (3, 4):
 
 
 setup(name='pywintrace',
-      version='0.4.0',
+      version='0.5.0',
       description='ETW Tracing',
       author='Anthony Berglund',
       url='https://github.com/fireeye/pywintrace',
-      download_url='https://github.com/fireeye/pywintrace/archive/v0.3.0.tar.gz',
+      download_url='https://github.com/winterknife/pywintrace/archive/v0.5.0.tar.gz',
       platforms=['Windows'],
       license='Apache',
       packages=['etw'],
